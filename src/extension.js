ff35bdae0881684f20b8cf5aa06ec37e2e238f2e
ArturoDent <31813701+ArturoDent@users.noreply.github.com>
2023-01-25 18:33:25 -0700

    - inital commit

---
+++ src/extension.js
@@ -0,0 +1,57 @@
+const vscode = require('vscode');
+
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {
+  
+  const workspaceFiles = await vscode.workspace.findFiles('**');
+  const fileDecorationProvider = await createDecoratorClass(workspaceFiles);  // runs on start-up
+}
+
+/**
+ *
+ *
+ * @param {vscode.Uri[]} workspaceFiles
+ * @returns FileDecorationProvider
+ **/
+async function createDecoratorClass(workspaceFiles) {
+  
+  class FileDecorationProvider {
+    constructor() {
+      this.disposables = [];
+      this.disposables.push(vscode.window.registerFileDecorationProvider(this));
+    }
+    
+    async provideFileDecoration(uri) {
+      
+      const isFile = await vscode.workspace.fs.stat(uri);
+      
+      const result = workspaceFiles.findIndex(file => file.fsPath === uri.fsPath);
+      
+      if (isFile.type === 1 && result < 0) return {
+        badge: "!!",
+        color: new vscode.ThemeColor("highlightFiles.nonWorkspaceFiles"),
+        tooltip: "File not in workspace"
+      };
+    }
+
+    dispose() {
+      this.disposables.forEach((d) => d.dispose());
+    }
+  }
+  
+  return new FileDecorationProvider();
+}
+
+// exports.activate = activate;
+
+function deactivate() {
+  this.fileDecorationProvider.dispose();
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
